f30def71989b58fa4cb5adb7d726ebdd0dd62688
Magons <vano47@bk.ru>
2016-03-25 10:12:42 +0300

    #coffe in js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,42 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(function() {
+  $('#collection_small_icon').on('change', function(event) {
+    var image, reader;
+    image = event.target.files[0];
+    reader = new FileReader;
+    reader.onload = function(file) {
+      return $('#small_icon').html("<img class='img-rounded' src='" + file.target.result + "' style='width: 140px; height: 140px;'>");
+    };
+    return reader.readAsDataURL(image);
+  });
+  $('#collection_small_icon_2').on('change', function(event) {
+    var image, reader;
+    image = event.target.files[0];
+    reader = new FileReader;
+    reader.onload = function(file) {
+      return $('#small_icon_2').html("<img class='img-rounded' src='" + file.target.result + "' style='width: 140px; height: 140px;'>");
+    };
+    return reader.readAsDataURL(image);
+  });
+  $('#collection_large_icon').on('change', function(event) {
+    var image, reader;
+    image = event.target.files[0];
+    reader = new FileReader;
+    reader.onload = function(file) {
+      return $('#large_icon').html("<img class='img-rounded' src='" + file.target.result + "' style='width: 140px; height: 140px;'>");
+    };
+    return reader.readAsDataURL(image);
+  });
+  return $('#collection_large_icon_2').on('change', function(event) {
+    var image, reader;
+    image = event.target.files[0];
+    reader = new FileReader;
+    reader.onload = function(file) {
+      return $('#large_icon_2').html("<img class='img-rounded' src='" + file.target.result + "' style='width: 140px; height: 140px;'>");
+    };
+    return reader.readAsDataURL(image);
+  });
+});
