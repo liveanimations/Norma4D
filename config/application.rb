0a5008941e5e839a8cd77062afbc89490c6f2c7f
Magons <vano47@bk.ru>
2016-03-25 10:24:37 +0300

    Array

--- config/application.rb
+++ config/application.rb
@@ -20,10 +20,10 @@ module Norma4D
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.assets.precompile += ['collections.coffee', 'effects.coffee']
+    # Do not swallow errors in after_commit/after_rollback callbacks
+    config.assets.precompile += ['collections.js', 'effects.css']
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = true
-    config.serve_static_assets = true
+    config.serve_static_files = true
   end
 end
