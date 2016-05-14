6b0fb21846bb7f416a0c8614e915a270db5fea84
Magons <vano47@bk.ru>
2016-05-14 14:36:09 +0300

    Change directorry for commercials attacments

--- app/models/commercial.rb
+++ app/models/commercial.rb
@@ -1,8 +1,12 @@
 class Commercial < ActiveRecord::Base
   belongs_to :application
   belongs_to :collection
-  has_attached_file :image1, styles: { medium: '300x300>', thumb: '140x140>' }
-  has_attached_file :image2, styles: { medium: '300x300>', thumb: '140x140>' }
+  has_attached_file :image1, styles: { medium: '300x300>', thumb: '140x140>' },
+                    url: '/files/commercials/:id/image1/:style/image1.:extension',
+                    path: ':rails_root/public/files/commercials/:id/image1/:style/image1.:extension'
+  has_attached_file :image2, styles: { medium: '300x300>', thumb: '140x140>' },
+                    url: '/files/commercials/:id/image2/:style/image2.:extension',
+                    path: ':rails_root/public/files/commercials/:id/image2/:style/image2.:extension'
 
   validates :name, presence: true
   # validates_attachment_presence :image1
