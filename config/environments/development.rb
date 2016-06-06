5a345fe15b025c9e081f1e6a8cc59dc44776f226
Magons <vano47@bk.ru>
2016-06-07 00:08:15 +0300

    #Delete mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,16 +35,8 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => "587",
-    :domain => "gmail.com",
-    :user_name => "vanyaprokopenko@gmail.com",
-    :password => "43648597q",
-    :authentication => "plain",
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings = { :api_token => "dd" }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
