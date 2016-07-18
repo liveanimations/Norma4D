142fe265088e86dc377cde9553fc81b1ca4cb0b5
Magons <vano47@bk.ru>
2016-07-18 12:15:00 +0300

    Change avaliable scope

--- app/models/effect.rb
+++ app/models/effect.rb
@@ -38,5 +38,5 @@ class Effect < ActiveRecord::Base
   validates_attachment_presence :large_icon_2
   validates_attachment_content_type :small_icon, :small_icon_2, :large_icon, :large_icon_2,
   content_type: /\Aimage\/.*\Z/
-  scope :avaliable, -> { where(hide: false) }
+  scope :avaliable, -> { joins(:collection).where('collections.hide = false').where(hide: false) }
 end
