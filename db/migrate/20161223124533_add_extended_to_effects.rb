878a992356b35581c954b549b852b1b5bf6a2b7a
Magons <vano47@bk.ru>
2017-03-23 23:26:47 +0300

    Remove migrations

--- db/migrate/20161223124533_add_extended_to_effects.rb
+++
@@ -1,5 +0,0 @@
-class AddExtendedToEffects < ActiveRecord::Migration
-  def change
-    add_column :effects, :extended, :boolean, default: false
-  end
-end
--- db/migrate/20170315055654_add_app_version_to_app.rb
+++
@@ -1,6 +0,0 @@
-class AddAppVersionToApp < ActiveRecord::Migration
-  def change
-    add_column :applications, :version_android, :string
-    add_column :applications, :version_ios, :string
-  end
-end
