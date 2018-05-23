29cb29138acf70304791b7535b95c3e4b4909340
Magons <vano47@bk.ru>
2018-05-23 20:47:30 +0300

    Stop redirect to India VPS

--- app/controllers/concerns/india_redirectable.rb
+++ app/controllers/concerns/india_redirectable.rb
@@ -5,23 +5,23 @@ module IndiaRedirectable
 
     private
     def india_redirectable(path_regexp)
-      before_action do
-        if Rails.env.production?
-          remote_ip = request.remote_ip
-          if remote_ip == '115.124.99.87'
-            logger.info 'India server client connection'
-          else
-            if request.path.match(path_regexp)
-              geo = MaxmindDB.lookup(remote_ip)
-              if geo.found? && geo.country.name.in?(%w(India))
-                logger.info "This client (IP: #{remote_ip}, country: #{geo.country.name}) is redirected to India VPS"
-                # redirect_to "https://india-api.liveanimations.org#{request.path}"
-                redirect_to "http://115.124.99.87#{request.path}"
-              end
-            end
-          end
-        end
-      end
+      # before_action do
+      #   if Rails.env.production?
+      #     remote_ip = request.remote_ip
+      #     if remote_ip == '115.124.99.87'
+      #       logger.info 'India server client connection'
+      #     else
+      #       if request.path.match(path_regexp)
+      #         geo = MaxmindDB.lookup(remote_ip)
+      #         if geo.found? && geo.country.name.in?(%w(India))
+      #           logger.info "This client (IP: #{remote_ip}, country: #{geo.country.name}) is redirected to India VPS"
+      #           # redirect_to "https://india-api.liveanimations.org#{request.path}"
+      #           redirect_to "http://115.124.99.87#{request.path}"
+      #         end
+      #       end
+      #     end
+      #   end
+      # end
     end
   end
 
